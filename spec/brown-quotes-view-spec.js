6a8202ae8beb63b42a6fc698571fff41517f2a15
Prajwal <2017ugcs059@nitjsr.ac.in>
2019-05-25 08:20:39 +0530

    Initial commit

---
+++ spec/brown-quotes-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import BrownQuotesView from '../lib/brown-quotes-view';
+
+describe('BrownQuotesView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
